e0170e7b9672717682c41692e15572324af1ec73
Raaz <21ketanraaz@gmail.com>
2023-04-14 22:00:01 +0530

    +wrapping the containers into scrollable widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,54 +10,61 @@ TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(children: transactions.map((tx) {
-      return Card(
-        child: Row(children: <Widget>[
-          Container(
-            margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 15
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.purple,
-                width: 2,
-              ),
-            ),
-            padding: const EdgeInsets.all(10),
-            child: Text('₹${tx.amount}',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple
-              ),
-            ),
-          ),
-          Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(tx.title.toString(),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.purple,
+    return  Container(
+      height: 300,
+
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+          return Card(
+            child: Row(children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Text('₹${tx.amount}',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple
+                  ),
                 ),
               ),
-              Text(
-                DateFormat.yMMMd().format(tx.date as DateTime),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 13,
-                  color: Colors.grey,
-                ),
-              ),
+              Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(tx.title.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.purple,
+                    ),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(tx.date as DateTime),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
+            ),
+          );
+        }).toList(),
         ),
-      );
-    }).toList(),
+      ),
     );
   }
 }
